64af59ac1a4cb59129ee4e5ff93289d83b5fdf7c
Youssef-emad000 <ye2224050@gmail.com>
2025-05-13 21:52:02 +0300

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/dice_roller.dart';
+
+
+// var startAligenment = Alignment.topCenter;
+// var endAligement = Alignment.bottomCenter;
+//
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({ required this.color1, required this.color2, super.key});
+//
+//   final Color color1;
+//   final Color color2;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(
+//             colors:[color1,color2],
+//             begin:startAligenment ,
+//             end: endAligement,
+//           ),
+//         ),
+//         child: const Center(
+//           child: DiceRoller(),
+//         ),
+//       ),
+//     );
+//   }
+// }
